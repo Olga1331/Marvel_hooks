6ea1030f9e70b24360390278b7276aa2ed9feceb
Olga <chadaeva88@gmail.com>
2021-10-14 11:43:22 +0300

    deploy heroku

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,13 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
-import { MainPage, ComicsPage } from "../pages";
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from "../spinner/Spinner";
 
+const Page404 = lazy(() => import ("../pages/404")); /* используем экспорт по умолчанию для ленивой подгрузки стр. */
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SinglComicPage'));
 
 const App = () => {
 
@@ -11,14 +16,22 @@ const App = () => {
                 <div className="app">
                 <AppHeader/>
                 <main>
-                <Switch>
-                    <Route exact path="/">
-                        <MainPage/>
-                    </Route>
-                    <Route exact path="/comics">
-                        <ComicsPage/>
-                    </Route>
-                </Switch>
+                <Suspense fallback={<Spinner/>}>
+                    <Switch>
+                        <Route exact path="/">
+                            <MainPage/>
+                        </Route>
+                        <Route exact path="/comics">
+                            <ComicsPage/>
+                        </Route>
+                        <Route exact path="/comics/:comicId">
+                            <SingleComicPage/>
+                        </Route>
+                        <Route path="*">
+                            <Page404/>
+                        </Route>
+                    </Switch>
+                </Suspense>
                 </main>
             </div>
             </Router>
